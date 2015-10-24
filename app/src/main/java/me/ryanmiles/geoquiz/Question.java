97c4f85e4fc5693cf74a709038a7b4119d61b697
ryanm14 <ryanm1114@gmail.com>
2015-10-24 13:06:37 -0400

    Fix

---
+++ app/src/main/java/me/ryanmiles/geoquiz/Question.java
@@ -0,0 +1,32 @@
+package me.ryanmiles.geoquiz;
+
+/**
+ * Created by ryanm14 on 10/24/15
+ */
+public class Question {
+
+    private int mTextResId;
+    private boolean  mAnswerTrue;
+
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean mAnswerTrue) {
+        this.mAnswerTrue = mAnswerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int mTextResId) {
+        this.mTextResId = mTextResId;
+    }
+
+    public Question(int mTextResId, boolean mAnswerTrue) {
+        this.mTextResId = mTextResId;
+        this.mAnswerTrue = mAnswerTrue;
+    }
+}
